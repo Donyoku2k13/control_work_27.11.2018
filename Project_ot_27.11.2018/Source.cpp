2acfc0c4be8473ac543d2db557993bab288d55b4
Александр Горячев <@local.itstep>
2018-11-27 20:34:49 +0600

    Добавьте файлы проекта.

---
+++ Project_ot_27.11.2018/Source.cpp
@@ -0,0 +1,180 @@
+#include <stdio.h>
+#include <iostream>
+#include<locale.h>
+#include<math.h>
+int main()
+{
+	setlocale(LC_ALL, "");
+	
+	/*int N;
+	printf("N:  ");
+	scanf_s("%d", &N);
+
+	if (N % 2 == 0 && N % 7 ==0 && N %11 >0 && N%13 >0)
+	{
+		printf("N:True\n");
+	}
+
+	else
+	{
+		printf("N:False\n");
+	}*/
+
+
+
+	/*task7b,c*/
+	/*int A, B;
+	printf("A:  ");
+	scanf_s("%d", &A);
+	printf("B:  ");
+	scanf_s("%d", &B);
+	if (A%2==0)
+	{
+		printf("B:четное\n");
+	}
+
+	else
+	{
+		printf("B: нечетное\n");
+	}*/
+
+
+
+
+
+	/*task7a*/
+	/*int n;
+	printf("n:  ");
+	scanf_s("%d", &n);
+
+	if (n > 9 && n < 100 && n%2==0)
+	{
+		printf("True \n");
+	}
+	else
+	{
+		printf("N:False\n");
+	}*/
+
+
+
+	/*task6*/
+	/*float S, H, M ;
+	printf("S:  ");
+	scanf_s("%f", &S);
+	M = S / 60;
+	H = M / 60;
+
+
+
+	printf("Это %f\n часов\n  %f\n минут",H, M);*/
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	/*task5*/
+	/*float r, p, ss, ss2;
+	printf("p:  ");
+	scanf_s("%f", &p);
+
+	printf("ss:  ");
+	scanf_s("%f", &ss);
+
+	ss2 = ss - (ss / 100 * 5);
+	r = p / ss2 * 100;
+
+	printf("%f\n", r);*/
+
+
+
+
+
+
+	/*task4*/
+	
+	/*int C;
+	printf("C:  ");
+	scanf_s("%d", &C);
+
+	if (C>400 && C<600)
+	{
+		printf("Sell=5% \n");
+	}
+
+
+	else if (C>600 && C<1000)
+	{
+		printf("Sell=10% \n");
+	}
+
+	else
+	{
+		printf("N:False\n");
+	}
+*/
+
+
+
+	/*task3*/
+	
+	/*int N;
+	printf("N:  ");
+	scanf_s("%d", &N);
+	
+	if (N % 2 == 1)
+	{
+		printf("no results \n");
+	}
+
+
+	else if (N % 3 == 1 && N % 7== 0)
+	{
+	 printf("N:True\n");
+    }
+	
+	else 
+	{
+		printf("N:False\n");
+	}
+	*/
+
+
+
+
+
+	/*task2*/
+	
+	/*printf("Name: PP-12-1 \n");
+	printf("creator: student  \n ");
+
+	printf("Size 50MB \n");
+	printf("Path: D:\Academy\PP-12-1 \n");*/
+
+
+
+
+	/*task1*/
+	/*printf("Name: 'The war and the peace' \n" );
+	printf("Avtor: L.N. Tolstoj  \n " );
+
+	printf("Izdatelstvo: Piter \n" );
+	printf("Pages: 500 \n");*/
+
+
+
+
+
+
+
+
+
+
+
+	system("pause");
+}
